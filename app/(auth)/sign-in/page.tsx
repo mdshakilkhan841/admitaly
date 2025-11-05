33188dbd01b07b1b194969b3429bcf7dcbf81ad3
mdshakilkhan841 <blc.apps2@daffodilvarsity.edu.bd>
2025-11-05 18:02:40 +0600

    feat: Added padding

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -44,7 +44,7 @@ const SigninPage = () => {
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-50">
+        <div className="flex items-center justify-center min-h-screen bg-gray-50 p-4">
             <Card className="w-full max-w-sm">
                 <CardHeader className="text-center">
                     <CardTitle className="text-2xl">Sign In</CardTitle>
